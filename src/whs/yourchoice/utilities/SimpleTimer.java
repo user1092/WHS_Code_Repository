0185e3f3988f877cf12a10618a5e3053a4d3c280
user1092 <ch1092@york.ac.uk>
2016-04-06 16:17:25 +0100

    Fixed so time can run and stop, pause is broken

--- src/whs/yourchoice/utilities/SimpleTimer.java
+++ src/whs/yourchoice/utilities/SimpleTimer.java
@@ -4,29 +4,69 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 /**
+* Class for creating a simple timer that returns true when complete or false when cancelled.
 * 
-* @author user828 & user1092
-* @version v0.1 28/03/16
+* @author cd828 & ch1092
+* @version v0.1 06/04/16
 */
-
 public class SimpleTimer{
 	
-
+	private Timer timer;
 	private Boolean timerDone = false;
+	private Boolean cancelled = false;
 	
-	public boolean timer(Integer time) {
-		Timer timer = new Timer();
+	
+	/**
+	 * Method to start a timer incrementing to the specified time.
+	 * 
+	 * @param time	-	The time for the timer to increment to
+	 * @return			true if the timer succeeded, false if the timer was cancelled	
+	 */
+	public Boolean startTimer(Integer time) {
+		timer(time);
+		while(!timerDone && !cancelled);
+		return timerDone;
+		
+	}
+	
+	
+	/**
+	 * Method to start a TimerTask setting timerDone true when complete.
+	 * 
+	 * @param time	-	The amount of time the timer should run before timerDone is set true
+	 */
+	private void timer(Integer time) {
+		timer = new Timer(true);
 		timer.schedule(new TimerTask() {
 			@Override
 			public void run() {
 				timerDone = true;
 			}
 		}, time);
-		while(!timerDone);
-		return timerDone;
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+	/**
+	 * Method to stop the running timer.
+	 */
+	public void stopTimer() {
+		cancelled = true;
+		timer.cancel();
 	}
+
+
+	public void pauseTimer() throws InterruptedException {
+		System.out.println("tell to pause");
+		timer.wait();
+		System.out.println("paused");
+	}
+
+
+	public void resumeTimer() {
+		System.out.println("tell to unpause");
+		timer.notifyAll();
+		System.out.println("unpaused");
+	}
+	
+	
 }
--- src/whs/yourchoice/utilities/SimpleTimerTest.java
+++ src/whs/yourchoice/utilities/SimpleTimerTest.java
@@ -1,27 +1,143 @@
 package whs.yourchoice.utilities;
 
 import static org.junit.Assert.*;
-
-import java.util.Timer;
-
 import org.junit.Test;
 
+/**
+ * Class for testing the SimpleTimer class.
+ * 
+ * NOT FOR RELEASE!
+ * 
+ * @author		ch1092, cd828
+ * @version		v0.1 06/04/2016
+ */
 public class SimpleTimerTest {
 	
-	Integer time = 12000;
+	SimpleTimer timer;
 	
-	boolean isTimerDone;
+	Boolean timerDone;
 	
+	Integer runTime = 12000;
+	Integer interactTime = 6000;
+	Integer upperLimit;
+	Integer lowerLimit;
+	
+	Long timeTaken;
+	
+	
+	Double tenPercentOfRunTime;
+	Double tenPercentOfCancelTime;
+	
+	
+	/**
+	 * Test that the timer runs for the specified time and returns true.
+	 */
 	@Test
-	public void setup() {
+	public void timerShouldRunForSpecifiedTime() {
 
-		long startTime = System.nanoTime();
-		System.out.println(startTime);
-		SimpleTimer timer = new SimpleTimer();
-		while(timer.timer(time));
-		long endTime = System.nanoTime();
-		System.out.println(endTime);
+		Long startTime = System.nanoTime();
 		
-		//assertEquals((endTime - startTime), time);
+		timer = new SimpleTimer();
+		
+		timerDone = timer.startTimer(runTime);
+		
+		Long endTime = System.nanoTime();
+		
+		timeTaken = ((endTime - startTime) / 1000000);
+		
+		tenPercentOfRunTime = (runTime * 0.1);
+		
+		upperLimit = runTime + Integer.valueOf(tenPercentOfRunTime.intValue());
+		
+		lowerLimit = runTime - Integer.valueOf(tenPercentOfRunTime.intValue());
+		
+		assertTrue(Integer.valueOf(timeTaken.intValue()) >= lowerLimit);
+		assertTrue(Integer.valueOf(timeTaken.intValue()) <= upperLimit);
+		assertEquals(true, timerDone);
+	}
+	
+	
+	/**
+	 * Tests that a timer can be cancelled and returns false.
+	 */
+	@Test
+	public void timerShouldBeAbleToBeCancelled() {
+
+		Long startTime = System.nanoTime();
+		
+		timer = new SimpleTimer();
+		
+		// Thread to cancel timer under test after interactTime
+		Thread cancelThread = new Thread("Cancel") {
+			public void run() {
+				SimpleTimer cancelTimer = new SimpleTimer();
+				cancelTimer.startTimer(interactTime);
+				timer.stopTimer();
+			}
+		};
+		cancelThread.start();
+		
+		timerDone = timer.startTimer(runTime);
+		
+		Long endTime = System.nanoTime();
+		
+		timeTaken = ((endTime - startTime) / 1000000);
+		
+		tenPercentOfCancelTime = (interactTime * 0.1);
+		
+		upperLimit = interactTime + Integer.valueOf(tenPercentOfCancelTime.intValue());
+		
+		lowerLimit = interactTime - Integer.valueOf(tenPercentOfCancelTime.intValue());
+		
+		assertTrue(Integer.valueOf(timeTaken.intValue()) >= lowerLimit);
+		assertTrue(Integer.valueOf(timeTaken.intValue()) <= upperLimit);
+		assertEquals(false, timerDone);
+	}
+	
+	
+	/**
+	 * Tests that a timer can be pause and resumed and returns true.
+	 */
+	@Test
+	public void timerShouldBeAbleToBePaused() {
+
+		Long startTime = System.nanoTime();
+		
+		timer = new SimpleTimer();
+		
+		// Thread to pause timer under test after interactTime
+		Thread pauseThread = new Thread("Pause") {
+			public void run() {
+				SimpleTimer pauseTimer = new SimpleTimer();
+				pauseTimer.startTimer(interactTime);
+				try {
+					timer.pauseTimer();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				pauseTimer.startTimer(interactTime);
+				timer.resumeTimer();
+			}
+		};
+		pauseThread.start();
+		
+		timerDone = timer.startTimer(runTime);
+		
+		Long endTime = System.nanoTime();
+		
+		timeTaken = ((endTime - startTime) / 1000000);
+		
+		tenPercentOfCancelTime = (interactTime * 0.1);
+		
+		upperLimit = interactTime + runTime 
+						+ Integer.valueOf(tenPercentOfCancelTime.intValue());
+		
+		lowerLimit = interactTime + runTime 
+						- Integer.valueOf(tenPercentOfCancelTime.intValue());
+		
+		assertTrue(Integer.valueOf(timeTaken.intValue()) >= lowerLimit);
+		assertTrue(Integer.valueOf(timeTaken.intValue()) <= upperLimit);
+		assertEquals(true, timerDone);
 	}
 }
