70016d11fb6adc56e68b6fdd9ca86a4c1fcc9afb
user1092 <ch1092@york.ac.uk>
2016-05-30 14:32:55 +0100

    update to image handler

--- src/stammtisch/handlers/ImageHandler.java
+++ src/stammtisch/handlers/ImageHandler.java
@@ -69,10 +69,11 @@ public class ImageHandler
 		return ((sizeOfCanvasY*imageToHandle.getHeight()));
 	}
 	
+	//PATCH:  30th May 2016 added in the "file:" string to allow for non repository images.
 	//this method will read the source file and create the image
 	private Image generateImage() 
 	{
-		return new Image(imageToHandle.getSourceFile());
+		return new Image("file:" + imageToHandle.getSourceFile());
 	}
 
 }
